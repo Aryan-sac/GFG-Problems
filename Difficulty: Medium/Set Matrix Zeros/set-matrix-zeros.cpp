40a43bd08d0c84202246f5db7c82320523b94c87
Aryan-sac <aryansachan825@gmail.com>
2026-02-16 16:29:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    void setMatrixZeroes(vector<vector<int>> &matrix) {
+        // code here
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(matrix[i][j] == 0) {
+                    // mark row i
+                    for(int a=0; a<n; a++) {
+                        if(matrix[i][a] != 0) matrix[i][a] = -100000;
+                    }
+                    // mark column j
+                    for(int a=0; a<m; a++) {
+                        if(matrix[a][j] != 0) matrix[a][j] = -100000;
+                    }
+                }
+            }
+        }
+        
+        // convert markers to zero
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(matrix[i][j] == -100000) matrix[i][j] = 0;
+            }
+        }
+    }
+};
